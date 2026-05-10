0c0570083e63bbb60a8384a311da09a7ddf6f388
Sadik Ahsan <sadikahasan6@gmail.com>
2026-05-10 07:03:28 +0600

    Fix: windows terminal leaf icon issue

--- src/main.zig
+++ src/main.zig
@@ -1,6 +1,9 @@
 const std = @import("std");
 const net = std.Io.net;
 const conn = @import("connection.zig");
+const builtin = @import("builtin");
+
+extern "kernel32" fn SetConsoleOutputCP(wCodePageID: u32) callconv(.winapi) u32;
 
 pub const HOST = "127.0.0.1";
 pub const PORT = 8080;
@@ -8,6 +11,10 @@ pub const PORT = 8080;
 pub fn main(init: std.process.Init) !void {
     const io = init.io;
     const gpa = init.gpa;
+
+    if (builtin.os.tag == .windows) {
+        _ = SetConsoleOutputCP(65001);
+    }
 
     const addr = try net.IpAddress.parse(HOST, PORT);
     var listener = try addr.listen(io, .{ .reuse_address = true });
